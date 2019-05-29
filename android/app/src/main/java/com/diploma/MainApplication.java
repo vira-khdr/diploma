a5cf1e40f8ef690cb72d05780ea35c2d2455bffe
Vira Rodionova <bepa.rdnv@gmail.com>
2019-05-30 00:40:15 +0300

    install react native workers

--- android/app/src/main/java/com/diploma/MainApplication.java
+++ android/app/src/main/java/com/diploma/MainApplication.java
@@ -3,6 +3,7 @@ package com.diploma;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.fabricio.vergal.RNWorkers.RNWorkersPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNWorkersPackage(),
           new RNCWebViewPackage(),
           new RNThreadPackage(mReactNativeHost)
       );
